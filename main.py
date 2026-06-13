571d6710817288e2d2ba810dac2aa269e2385db0
mommaloveme39-ui <mommaloveme39@gmail.com>
2026-06-13 05:37:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import telebot
 
 # 1. Add your token
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-bot = telebot.TeleBot(BOT_TOKEN)
+8773341112:AAGu5MuAFZhwtKJNeyOQ1L6Wl72q1gT52mwbot = telebot.TeleBot(BOT_TOKEN)
 
 # 2. Your bot's commands go here
 @bot.message_handler(commands=['start'])
