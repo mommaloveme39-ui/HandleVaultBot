a1b0580e5a375176c3da626e945ce547c1508751
mommaloveme39-ui <mommaloveme39@gmail.com>
2026-06-13 06:07:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 import telebot
 
-# 1. Add your token
-BOT_TOKEN = "8773341112:AAGu5MuAFZhwtKJNeyOQ1L6Wl72q1gT52mw"
+# Paste your BRAND NEW token here
+8773341112:AAEj1r5HOfbqUJrwaKk9kmFTjvL2aMeJUiw
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Hello! The bot is working.")
 
-# 3. Keep the bot running
 print("Starting bot polling...")
 bot.infinity_polling()
