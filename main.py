becbbde2b48728a631fe2eda6206dc371340969a
mommaloveme39-ui <mommaloveme39@gmail.com>
2026-06-13 05:52:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import telebot
 
 # 1. Add your token
-8773341112:AAGu5MuAFZhwtKJNeyOQ1L6Wl72q1gT52mw
-# 2. Your bot's commands go here
+8773341112:AAGu5MuAFZhwtKJNeyOQ1L6Wl72q1gT52mw# 2. Your bot's commands go here
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Hello! The bot is working.")
